8bb817ed9adbf7eb1d40241d02e2b109c10a463b
Jeffrey Ymor Calayag <jeffreyymor.calayag@gmail.com>
2023-02-11 07:16:56 +0800

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "../components/Heading.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <form></form>
+    </div>
+  );
+}
+
+export default App;
