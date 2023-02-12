7226b2154bc879c6f4d9ce05caf99296db8ad036
Jeffrey Ymor Calayag <jeffreyymor.calayag@gmail.com>
2023-02-12 18:08:29 +0800

    Add map, to show the notes list inside the allNotes array/state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "../components/Heading.jsx";
 import Footer from "../components/Footer.jsx";
 import Note from "../components/Note.jsx";
 import CreateNote from "../components/CreateNote.jsx";
 
 function App() {
+  const [allNotes, setAllNotes] = useState([]);
+
+  function handleSubmit(note) {
+    setAllNotes((prevValue) => {
+      return [...prevValue, note];
+    });
+    console.log(allNotes);
+  }
+
   return (
     <div>
       <Heading />
-      <CreateNote />
-      <Note />
+      <CreateNote onAdd={handleSubmit} />
+      {allNotes.map((singleNote, index) => {
+        return (
+          <Note
+            key={index}
+            noteTitle={singleNote.title}
+            noteContent={singleNote.content}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateNote() {
+function CreateNote(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleNoteInput(evt) {
+    const fieldName = evt.target.name;
+    const fieldValue = evt.target.value;
+
+    setNote((prevValue) => {
+      if (fieldName === "title") {
+        return {
+          title: fieldValue,
+          content: prevValue.content,
+        };
+      } else if (fieldName === "content") {
+        return {
+          title: prevValue.title,
+          content: fieldValue,
+        };
+      }
+    });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Type note here" rows="3" />
-        <button>Add</button>
+        <input
+          onChange={handleNoteInput}
+          name="title"
+          placeholder="Title"
+          value={note.title}
+        />
+        <textarea
+          onChange={handleNoteInput}
+          name="content"
+          placeholder="Type note here"
+          rows="3"
+          value={note.content}
+        />
+        <button
+          onClick={(evt) => {
+            evt.preventDefault();
+            props.onAdd(note);
+            setNote({
+              title: "",
+              content: "",
+            });
+          }}
+        >
+          Add
+        </button>
       </form>
     </div>
   );
